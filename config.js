9a796be7f5ce0cb9f20624e64921c8f98b870032
Ryan McCarter <mccarter.ryan@gmail.com>
2015-02-02 15:41:28 -0800

    (fix) adds config.js file

--- config.js
+++ config.js
@@ -1,10 +1,9 @@
- //
- var exports = module.exports = {};
+var exports = module.exports = {};
 
-exports.GITHUB_CLIENT_ID = "5490ca3123aa702c0b5f";
-exports.GITHUB_CLIENT_SECRET = "d8a9ca8a81b6ba68156fe46864caf809de2b2b5d";
+exports.GITHUB_CLIENT_ID = //enter client ID here
+exports.GITHUB_CLIENT_SECRET = //enter secret here
 
-var password = 'rock'; //save your mySQL server root password in this variable if you have one
+var password = ''; //save your mySQL server root password in this variable if you have one
 exports.mysqlPassword = password || null; //defaults to null if no mySQL root password is provided
 
 exports.teachers = {
